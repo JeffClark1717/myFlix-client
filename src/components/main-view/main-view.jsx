075e76fb6a5b617e6ac490c4beb8f8e7d26213c3
Jeff Clark <116308210+JeffClark1717@users.noreply.github.com>
2023-07-25 19:35:28 -0700

    corrected import

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,9 +1,67 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
 import { MovieCard } from "../movie-card/movie-card.jsx";
 import { MovieView } from "../movie-view/movie-view.jsx";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
+    const [movies, setMovies] = useState([]);
+
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch('https://notflix1717-51672d8e0ed0.herokuapp.com/movies')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const moviesFromApi = data.map((movie) => {
+          return {
+            _id: movie.id,
+            title: movie.title,
+            image: movie.image,
+            description: movie.description,
+            genre: {
+              name: movie.genre.name
+            },
+            director: {
+              name: movie.director.name
+            },
+            featured: movie.featured.toString()
+          };
+        });
+        setMovies(moviesFromApi);
+      });
+  }, []);
+
+
+    if (selectedMovie) {
+        return (
+            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        ); 
+    }
+
+    if (movies.length === 0) {
+        return <div>This list is empty!</div>;
+    }
+
+    return (
+        <div>
+            {movies.map((movie) => (
+                <MovieCard
+                    key={movie.title}
+                    movie={movie}
+                    onMovieClick={(newSelectedMovie) => {
+                        setSelectedMovie(newSelectedMovie);
+                    }}
+                />
+            ))
+            }
+        </div>
+    );
+};
+
+
+
+
+   /* const [movies, setMovies] = useState([
         {
             id: "ObjectId('648a25b28d925836d83d5c21')",
             title: "Step Brothers",
@@ -28,32 +86,4 @@ export const MainView = () => {
             director: "Peter Farelly",
             genre: "Comedy"
         },
-    ]);
-
-    const [selectedMovie, setSelectedMovie] = useState(null);
-
-    if (selectedMovie) {
-        return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-        ); 
-    }
-
-    if (movies.length === 0) {
-        return <div>This list is empty!</div>;
-    }
-
-    return (
-        <div>
-            {movies.map((movie) => (
-                <MovieCard
-                    key={movie.id}
-                    movie={movie}
-                    onMovieClick={(newSelectedMovie) => {
-                        setSelectedMovie(newSelectedMovie);
-                    }}
-                />
-            ))
-            }
-        </div>
-    );
-};
+    ]);  */
